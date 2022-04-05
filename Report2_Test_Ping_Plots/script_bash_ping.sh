fd89724a0248e60befb9c60547b25a711a12696f
Lillo Barilotto <lillo@pop-os.localdomain>
2022-04-06 01:47:22 +0200

    add new .pdf about Dongle Ethernet

---
+++ Report2_Test_Ping_Plots/script_bash_ping.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if test -f "temp_result_ping.txt"; then
+    rm "temp_result_ping.txt"   
+fi
+
+echo "Size RttMin RttAvg RttMax" >> temp_result_ping.txt
+
+#Per il Dongle Ethernet e' stato utilizzata un'altra sequenza
+
+sequ=”$(seq 16 100 1450) $(seq 1470 1 1500) $(seq 1600 100 10000)”
+
+for i in $sequ; do
+    tmp=$(ping -c 10 -s $i -i 0.2 172.16.1.2 | grep "rtt" | cut -d ' ' -f 4 | cut -d '/' -f
+    1,2,3 --output-delimiter ' ')
+    echo $i $tmp >> temp_result_ping.txt
+done
